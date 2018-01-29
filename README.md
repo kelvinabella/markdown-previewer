b90ecfc914e2a455565fa11ddd76f79166bc2873
Kelvin Abella <abellavinkel@gmail.com>
2018-01-29 20:44:37 +0400

    initial commi

---
+++ README.md
@@ -0,0 +1,25 @@
+# Build a Markdown Previewer
+
+Full stack JavaScript service that allows you to preview markdown . A [FreeCodeCamp](https://www.freecodecamp.org/challenges/build-a-markdown-previewer) project.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Installing
+
+```
+git clone https://github.com/kelvinabella/markdown-previewer.git
+cd markdown-previewer
+yarn install
+yarn start
+```
+
+## License
+
+This project is licensed under the MIT License.
+
+## Acknowledgments
+
+*FreeCodeCamp
+*Made using Create React App
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+
+const marked = require("marked");
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const initialText =
+      "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nManila.\n\n *[Kelvin Abella](https://freecodecamp.com/kelvinabella)*";
+
+    this.state = {
+      value: initialText,
+      markedup: marked(initialText),
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value,
+      markedup: marked(e.target.value),
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <textarea
+          className="input"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <div className="output">
+          <div dangerouslySetInnerHTML={{ __html: this.state.markedup }} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
